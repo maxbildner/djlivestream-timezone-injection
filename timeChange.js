79885b95c28b1839ca7db23e40c4d3384da77f41
mbildner <mbildner@icloud.com>
2020-12-12 10:21:39 -0800

    fix PM time bug

--- timeChange.js
+++ timeChange.js
@@ -30,11 +30,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // grab EST time
     let estTime = time.innerHTML.split("&")[0];
     // estTime == "10a"
-
+    debugger
     // convert EST time to users timezone
     let newTime = formatAndConvertTZ(estTime);
     // newTime == "7a"
-    
+    debugger
+
     // replace inner span text with new time
     time.innerHTML = `${newTime}&nbsp`;
     // time == <span class="item-time item-time--12hr">7a&nbsp;</span>
@@ -42,7 +43,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function formatAndConvertTZ(est) {
     // est == "10a"   or "2:30p"
-    
+    let hours;
+    let minutes;
+    debugger
+    // convert est string to 24 hour string format ("20:00:00")
+    // if AM time
+    if (est[est.length - 1].toLowerCase() === "a") {
+      est = est.split(":");
+      // est == ["10a"]   or ["10", "30a"]
+      debugger
+      hours = est[0].slice(0, est[0].length - 1);
+      minutes = (est[1]) ? est[1].slice(0, est[1].length - 1) : "00";
+      est = `${hours}:${minutes}:00`;
+      // est == "8:00:00"   or "8:30:00"
+      debugger
+      // else PM time
+    } else {
+      est = est.split(":");
+      // est == ["8p"]   or ["8", "30p"]
+      debugger
+      hours = est[0].slice(0, est[0].length - 1);
+      minutes = (est[1]) ? est[1].slice(0, est[1].length - 1) : "00";
+      // hours == "8"     minutes == "30"
+      debugger
+      est = (12 + Number(est[0].slice(0, est[0].length - 1))) + ":" + minutes + ":00";
+      debugger
+    }
+
     // let userTime = convertTZ(`2020/12/3 ${est} GMT-0500`, timeZone).toString();
     // userTIme == "Tue Nov 03 2020 08:00:00 GMT-0500 (Eastern Standard Time)"
     let userTime = convertTZ(`2020/12/3 ${est} GMT-0500`, timeZone).toLocaleString();
@@ -62,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // remove extra zeros "00"
       userTime = userTime.split(":")[0];
       // userTime == "8"
-    } 
+    }
 
     userTime = userTime + ((isAM) ? 'a' : 'p');
     // userTime == "8a"
@@ -75,6 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // TO DO:
-  // 1) find out how to convert "10a" EST -> "7a" users time
-  // 2) find out how to change timezones of times on mouse hover blocks
+  // 1) find out how to change timezones of times on mouse hover blocks
+  // 2) Test Live
+  // 3) Test on various browsers
 });
